0cc37a56dacf12f693b27271490073186ad055db
Arisa Hirata <arisa.pirata@gmail.com>
2021-08-15 14:30:27 -0700

    Web Request API Part 1

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,8 @@
-// TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+  console.log(details)
+  return {
+    cancel: true
+  }
+}, {
+  urls: ['*://*.googleadservices.com/*', '*://*.tpc.googlesyndication.com/*', "*://*.g.doubleclick.net/*"],
+},['blocking'])
